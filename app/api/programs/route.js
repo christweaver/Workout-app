308b84dd1db37182a4386f2365c6b802f849fe59
christy <christyweaver.cw@gmail.com>
2023-10-05 17:16:48 -0500

    switched back to version that wont deploy

--- app/api/programs/route.js
+++ app/api/programs/route.js
@@ -2,14 +2,6 @@ import connectToDb from "@/db/mongodb";
 import Program from "@/app/models/programSchema";
 import { NextResponse } from "next/server";
 
-export async function POST(req) {
-  const { name, reps, weight, sets, username } = await req.json();
-  console.log(req);
-  await connectToDb();
-  await Workout.create({ name, reps, weight, sets, username });
-  return NextResponse.json({ message: "NICE" });
-}
-
 export async function GET() {
   await connectToDb();
 
